6472a9a7f91963abce910ae7f3178eeae7e545d9
Michael Bianchi <michael@bianchi.dev>
2023-06-19 16:23:33 -0500

    simplify reg form

--- front/src/routes/register/+page.svelte
+++ front/src/routes/register/+page.svelte
@@ -1,67 +1,85 @@
 <script lang="ts">
-    import { goto } from '$app/navigation';
-    import { BASE_API_URI } from '$lib/constants';
+  import { goto } from "$app/navigation";
+  import { BASE_API_URI } from "$lib/constants";
+  import { writable } from 'svelte/store';
+  import { user } from "../../stores/user"
 
-    let email = "",
-        challenge: string,
-        salt: string,
-        error: string | null;
+  let email = "",
+    error: string | null;
 
-    const submitForm = async () => {
-        await fetch(`${BASE_API_URI}/registration/start`, {
-            method: 'POST',
-            mode: 'cors',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                email: email
-            })
-        })
-            .then((response) => {
-                if (response.status === 200) {
-                    // notificationData.set('Registration successful. Login now.');
-                    response.json().then((data) => {
-                        console.log(data);
-                        challenge = data.challenge;
-                        salt = data.salt;
-                    })
-                } else if (response.status === 400) {
-                    console.log(response.json());
-                } else {
-                    console.log(response.json());
-                }
-            })
-            .catch((error) => {
-                error = error;
-                console.error('Error:', error);
-            });
-    };
+  const challenge = writable("");
+
+  const emailRegex = /.+@.+\..+/;
+  const validateEmail = (email: string) => {
+    return !!email.match(emailRegex);
+  }
+
+
+
+  const submitForm = async () => {
+    await fetch(`${BASE_API_URI}/registration/start`, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+      }),
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          // notificationData.set('Registration successful. Login now.');
+          response.json().then((data) => {
+            console.log(data);
+            $user.email = email;
+            $user.salt = data.sale;
+            $challenge = data.challenge;
+            goto(`/register/confirm`)
+          });
+        } else if (response.status === 400) {
+          console.log(response.json());
+        } else {
+          console.log(response.json());
+        }
+      })
+      .catch((error) => {
+        error = error;
+        console.error("Error:", error);
+      });
+  };
 </script>
 
-
 <svelte:head>
-    <title>Register</title>
-    <meta name="description" content="User Registration" />
+  <title>Register</title>
+  <meta name="description" content="User Registration" />
 </svelte:head>
 
 <section class="container">
-    <h1>Register</h1>
-    {#if error}
-        <p class="center error">{error}</p>
-    {/if}
+  <h1>Register</h1>
+  {#if error}
+    <p class="center error">{error}</p>
+  {/if}
 
-    <form class="form" on:submit|preventDefault={submitForm}>
-        <input
-                bind:value={email}
-                name="email"
-                type="email"
-                aria-label="Email address"
-                placeholder="Email address"
-        />
-        <button class="btn" type="submit">Register</button>
-        <p>Challenge: {challenge}</p>
-        <p>Salt: {salt}</p>
-        <p class="center">Already a user? <a href="/register">Login</a>.</p>
-    </form>
+  <form class="form" on:submit|preventDefault={submitForm}>
+    <input
+      bind:value={$user.firstName}
+      name="firstName"
+      aria-label="First name"
+      placeholder="First name"
+      required
+    />
+    <input
+      bind:value={email}
+      name="email"
+      type="email"
+      aria-label="Email address"
+      placeholder="Email address"
+      required
+    />
+    <button class="btn" type="submit" disabled={!validateEmail(email)}>Submit</button>
+  </form>
+  <p>Challenge: {$challenge}</p>
+  <p>Salt: {$user.salt}</p>
+  <p class="center">Already a user? <a href="/login">Login</a>.</p>
 </section>
--- front/src/stores/auth.ts
+++
@@ -1,3 +0,0 @@
-import { writable, type Writable } from 'svelte/store'
-
-export const loginSession = <Writable<User>> writable(undefined)
