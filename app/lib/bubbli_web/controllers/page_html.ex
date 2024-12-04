26b9dea728e3bd315c82b6239f564f7fda933489
Bianchi <m@bianchi.dev>
2024-12-03 20:35:40 -0600

    Fresh liveview 1.0 app

---
+++ app/lib/bubbli_web/controllers/page_html.ex
@@ -0,0 +1,10 @@
+defmodule BubbliWeb.PageHTML do
+  @moduledoc """
+  This module contains pages rendered by PageController.
+
+  See the `page_html` directory for all templates available.
+  """
+  use BubbliWeb, :html
+
+  embed_templates "page_html/*"
+end
